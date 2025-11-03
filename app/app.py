0636bab2dc6f2d9f4e9c27da37af1eef25c8619d
mbrsagor <mbrsagor@gmail.com>
2025-11-03 14:54:54 +0600

    Implement POST endpoint for creating text posts and remove hello world endpoint

--- app/app.py
+++ app/app.py
@@ -1,9 +1,20 @@
+from turtle import pos
 from fastapi import FastAPI
+from app.schemas import PostCreateMode
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def say_hello():
-    return {"message": "Hello World"}
+text_posts = {
+    1: {'title': 'this is a simple post one', 'content': 'This is a simple text post content one'},
+    2: {'title': 'this is a simple post two', 'content': 'This is a simple text post content two'},
+    3: {'title': 'this is a simple post three', 'content': 'This is a simple text post content three'}
+}
+
+
+@app.post('/')
+def create_post(post: PostCreateMode):
+    new_post = {'title' : post.title, 'content': post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return text_posts
