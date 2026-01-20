fdce199cdd631a6c12243a367a0d273d0c3af890
mbrsagor <mbrsagor@gmail.com>
2026-01-20 09:28:09 +0600

    New: Day 4 challenge done

--- day4/main.tf
+++ day4/main.tf
@@ -1,12 +1,15 @@
 # Create instance
 
 resource "aws_instance" "example" {
-  count         = 1
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, replace with a valid one for your region
+  count         = var.instance_count
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
   region        = var.region
+  monitoring    = var.monitoring_enabled
+  associate_public_ip_address = var.associate_public_ip
 
   tags = {
-    Name = "ExampleInstance"
+    Environment = var.environment
+    Name        = "${var.environment}-EC2-Instance"
   }
 }
--- day4/variables.tf
+++ day4/variables.tf
@@ -1,5 +1,26 @@
+# Variables for AWS deployment region
 variable "region" {
   description = "AWS region to deploy resources into"
   type        = string
   default     = "us-east-1"
+}
+
+# New variable to specify the number of instances to create
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
+
+# New variable to specify the environment (e.g., dev, prod, preprod)
+variable "monitoring_enabled" {
+  description = "Enable monitoring for EC2 instances"
+  type        = bool
+  default     = true
+}
+
+# Associate public IP address variable
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address with the instance"
+  type        = bool
+  default     = true
 }
