bc2df2535a3c004e5fb792198f0a0c32269372ed
mbrsagor <mbrsagor@gmail.com>
2026-01-25 16:02:15 +0600

    New: Create Security group

--- day4/main.tf
+++ day4/main.tf
@@ -13,3 +13,25 @@ resource "aws_instance" "example" {
     Name        = "${var.environment}-EC2-Instance"
   }
 }
+
+
+# Create Security Group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "Allow TLS Security Group"
+  }
+}
+
+
+# Ingress Rule to allow TLS traffic from specified CIDR blocks
+resource "aws_vpc_security_group_ingress_rule", "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_viv4         = var.allowed_cidr_blocks
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+}
